8917c458e88d6e8c27e05fac1ea733946fb1b141
saurabhmishra20 <saurabhmish70@gmail.com>
2026-09-18 21:40:04 +0530

    Time: 4 ms (19.67%) | Memory: 42.1 MB (5.03%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -4,23 +4,14 @@ public:
         sort(nums.begin(), nums.end());
         //vector<int>arr;
         int n = nums.size();
-        int freq=1;
-        int ans=nums[0];
-        for(int i=1; i<n; i++)
+        int freq=0;
+        int ans=0;
+        for(int i=0; i<n; i++)
         {
-                if(nums[i]==nums[i-1])
-                {
-                     freq++;
-                }
-                else
-                {
-                    freq=1;
-                    ans=nums[i];
-                }
-                if(freq>n/2)
-                return ans;
+            if(freq==0) ans=nums[i];
+            if(ans==nums[i]) freq++;
+            else freq--;
         }
-           
         return ans;
     }
 };
